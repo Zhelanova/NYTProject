7285f561f6b5a72d9f3809e1c3f03996303087d4
Natalia <n_d_k@inbox.ru>
2026-06-02 02:11:15 +0300

    Initial Commit

---
+++ NYTProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYTProject
+//
+//  Created by Наталия Желанова on 02.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
